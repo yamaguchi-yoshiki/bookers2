76ca9b0b3dd8271cb16c7e15749d0b10d434b2c3
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-04-17 10:19:02 +0000

    [Add] 4-25 4/16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  # 未ログインでtopにリダイレクト
+  #before_action :authenticate_user!, except: [:top]
+  # devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resouce)
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resouce)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
+  
+  resources :users, only: [:index, :show, :edit, :update]
+  
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
