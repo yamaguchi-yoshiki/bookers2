8abfc940a7696676547031fabddbbb5bd06e2108
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-04-19 15:23:37 +0000

    [Add] 4-25 4/19

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,6 @@ class BooksController < ApplicationController
   def create
     @books = Book.all
     @new_book = current_user.books.new(book_params)
-    @user = User.find(current_user.id)
     if @new_book.save
       redirect_to book_path(@new_book.id), notice: "You have created book successfully."
     else
@@ -13,13 +12,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @new_book = Book.new
-    @user = User.find(current_user.id)
   end
 
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
-    @user = User.find(@book.user_id)
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @new_book = Book.new
-    @books = @user.books.all
+    @books = @user.books
   end
 
   def edit
